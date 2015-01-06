5c826900c9e78f875afa278bcab735201d3d0e51
AHN JAE-HA <eu81273@gmail.com>
2015-01-06 17:50:51 +0900

    update comments.

--- angular.snackbar.js
+++ angular.snackbar.js
@@ -11,7 +11,7 @@
 
 	var module = angular.module( "angular.snackbar", [] );
 
-	//스낵바 서비스
+	//snackbar service
 	module.factory("snackbar", ['$rootScope', function ($rootScope) {
 
 		return {
@@ -22,40 +22,40 @@
 		}
 	}]);
 
-	//다이렉티브
+	//snackbar directive
 	module.directive( "snackbar", ["$rootScope", "$compile", "$timeout", function($rootScope, $compile, $timeout) {
 
-		//스낵바가 보이는 시간
+		//snackbar duration time (ms)
 		var snackbarDuration = 3000;
 
-		//스낵바 엘리먼트 제거 시간
+		//delay time to remove from DOM after hide (ms)
 		var snackbarRemoveDelay = 200;
 
 		return function ( scope, element, attrs ) {
 
-			//초기화
+			//initialize
 			snackbarDuration = attrs.snackbarDuration || 3000;
 			snackbarRemoveDelay = attrs.snackbarRemoveDelay || 200;
 
 			$rootScope.$on('createSnackbar', function(event, received) {
 
-				//스낵바 템플릿
+				//snackbar template
 				var template = "<div class=\"snackbar snackbar-opened\"><span class=\"snackbar-content\">" + received.message + "</span></div>";
 
-				//스택바 템플릿 컴파일
+				//template compile
 				var snackbar = $compile(template)(scope);
 
-				//스낵바 추가
+				//add snackbar
 				angular.element(element).append(snackbar);
 
-				//스낵바 제거 타임아웃
+				//snackbar duration timeout
 				$timeout(function () {
-					//스냅바 감추기
+					//hide snackbar
 					return angular.element(snackbar).removeClass("snackbar-opened");
 
 				}, received.timeout || snackbarDuration).then(function (element) {
 
-					//스낵바 제거
+					//remove snackbar
 					$timeout( function () { element.remove(); }, snackbarRemoveDelay);
 				});
 			
